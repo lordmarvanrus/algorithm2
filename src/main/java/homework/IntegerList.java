52b77654c31aabafe3c9567ef3834e226ffa5638
Vladimir <marvalo73@gmail.com>
2022-09-13 16:41:13 +0700

    готово

--- src/main/java/homework/IntegerList.java
+++ src/main/java/homework/IntegerList.java
@@ -1,2 +1,31 @@
-package homework;public interface IntegerList {
+package homework;
+
+public interface IntegerList {
+        Integer add(Integer item);
+
+        Integer add(int index, Integer item);
+
+        Integer set(int index, Integer item);
+
+        Integer remove(Integer item);
+
+        Integer remove(int index);
+
+        boolean contains(Integer item);
+
+        int indexOf(Integer item);
+
+        int lastIndexOf(Integer item);
+
+        Integer get(int index);
+
+        boolean equals(IntegerList otherList);
+
+        int size();
+
+        boolean isEmpty();
+
+        void clear();
+
+        Integer[] toArray();
 }
